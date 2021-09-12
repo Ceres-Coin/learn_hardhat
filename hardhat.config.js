439d345177dcc96255fa288c91cc61769c3bc190
IlyaPrigogine <prigogine@protonmail.com>
2021-09-12 13:20:04 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,14 @@
 require("@nomiclabs/hardhat-waffle");
 const bsctestnet_PRIVATE_KEY = "";
 
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 module.exports = {
   solidity: "0.7.3",
   networks: {
