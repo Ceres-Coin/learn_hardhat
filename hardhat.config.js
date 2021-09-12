d19d7280649672ea4bac6f2f18acb82c02e15868
IlyaPrigogine <prigogine@protonmail.com>
2021-09-12 09:50:28 +0800

    ADDED DEPLOYMENT ON BSCTESTNET

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,14 @@
  */
 
 require("@nomiclabs/hardhat-waffle");
+const bsctestnet_PRIVATE_KEY = "";
 
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    bscTestnet: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545`
+      // accounts: [`0x${bsctestnet_PRIVATE_KEY}`],
+    },
+  },
 };
