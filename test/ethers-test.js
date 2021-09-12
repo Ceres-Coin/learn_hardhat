f9d0d8c975d6a614ef24e55e0b031411c62ab134
IlyaPrigogine <prigogine@protonmail.com>
2021-09-13 07:13:34 +0800

    Update ethers-test.js

--- test/ethers-test.js
+++ test/ethers-test.js
@@ -1,8 +1,15 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
+describe("test/ethers-test.js", function () {
   it("test001", async function () {
     console.log(`test001`)
+  });
+
+  it("test002", async function() {
+    const [acc0,acc1,acc2] = await ethers.getSigners();
+    console.log(`acc0: ${acc0.address}`);
+    console.log(`acc1: ${acc1.address}`);
+    console.log(`acc2: ${acc2.address}`);
   });
 });
