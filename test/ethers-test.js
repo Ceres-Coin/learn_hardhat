79105ccc4f587d51d03db2b0dbd2a4cb99e229f5
IlyaPrigogine <prigogine@protonmail.com>
2021-09-13 07:11:28 +0800

    submitted

---
+++ test/ethers-test.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Greeter", function () {
+  it("test001", async function () {
+    console.log(`test001`)
+  });
+});
