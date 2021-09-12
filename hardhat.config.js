b6166f45bf8fda4e77fafd7eec1dec65c55ab30a
IlyaPrigogine <prigogine@protonmail.com>
2021-09-13 07:01:18 +0800

    added hardhat tasks: "balance"

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@
  */
 
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
 const bsctestnet_PRIVATE_KEY = "";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -12,6 +13,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs) => {
+    const account = web3.utils.toChecksumAddress(taskArgs.account);
+    const balance = await web3.eth.getBalance(account);
+
+    console.log(web3.utils.fromWei(balance, "ether"), "ETH");
+  });
 
 module.exports = {
   solidity: {
